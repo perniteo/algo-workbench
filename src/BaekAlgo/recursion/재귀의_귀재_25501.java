455fa80ac963a9d6433b772c2c7c0c5d603348eb
perniteo <perniteo1@gmail.com>
2025-12-28 22:23:15 +0900

    auto: solved recursion

---
+++ src/BaekAlgo/recursion/재귀의_귀재_25501.java
@@ -0,0 +1,38 @@
+package BaekAlgo.recursion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 재귀의_귀재_25501 {
+
+  static int cnt = 0;
+
+  public static int recursion(String s, int l, int r){
+    cnt++;
+    if(l >= r) return 1;
+    else if(s.charAt(l) != s.charAt(r)) return 0;
+    else return recursion(s, l+1, r-1);
+  }
+
+  public static int isPalindrome(String s){
+    return recursion(s, 0, s.length()-1);
+  }
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 0; i < n; i++) {
+      String S = br.readLine();
+      cnt = 0;
+      sb.append(isPalindrome(S)).append(" ").append(cnt).append("\n");
+    }
+
+    System.out.println(sb);
+  }
+}
